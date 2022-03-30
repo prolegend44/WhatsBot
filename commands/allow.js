c70a9e2f95f67c4ea5c7d75a0713f4ce90fff55f
Dark-team-11 <102640547+Dark-team-11@users.noreply.github.com>
2022-03-30 11:05:14 +0530

    Update allow.js

--- commands/allow.js
+++ commands/allow.js
@@ -5,7 +5,7 @@ const pmpermit = require("../helpers/pmpermit");
 const execute = async (client, msg) => {
   if (config.pmpermit_enabled == "true" && !msg.to.includes("-")) {
     await pmpermit.permit(msg.to.split("@")[0]);
-    msg.reply("*✅ Allowed*\n\nYou are allowed for PM\n\n _Powered by WhatsBot_");
+    msg.reply("*Allowed*\n\nYou are allowed for PM\n\n _Powered by WhatsBot_");
   }
 };
 
@@ -15,6 +15,6 @@ module.exports = {
   command: "!allow",
   commandType: "admin",
   isDependent: false,
-  help: `_You can allow him for pm by these commands_ 👇\n*!allow* - Allow an user for PM\n*!nopm* - Disallow an allowed user for PM`, // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+  help: `_You can allow him for pm by these commands_ 👇\n*!allow* - Allow an user for PM\n*!disallow* - Disallow an allowed user for PM`, // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
   execute,
 };
